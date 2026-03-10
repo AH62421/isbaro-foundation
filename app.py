454ecc8caccffdbc0aad6a5cda0d362f315cb36d
AH62421 <abdalahersi12@gmail.com>
2026-03-10 13:56:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,39 +18,6 @@ def contact():
 def about():
     return render_template("about.html")
 
-@app.route("/vrijwilligers", methods=["GET","POST"])
-def vrijwilligers():
-
-    if request.method == "POST":
-
-        voornaam = request.form["voornaam"]
-        achternaam = request.form["achternaam"]
-        telefoon = request.form["telefoon"]
-        motivatie = request.form["motivatie"]
-
-        print("Nieuwe vrijwilliger:")
-        print(voornaam, achternaam)
-        print(telefoon)
-        from flask import Flask, render_template, request, redirect, url_for
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
 
 @app.route("/vrijwilligers", methods=["GET","POST"])
 def vrijwilligers():
@@ -62,7 +29,7 @@ def vrijwilligers():
         telefoon = request.form["telefoon"]
         motivatie = request.form["motivatie"]
 
-        with open("namen.txt", "a", encoding="utf-8") as file:
+        with open("vrijwilligers_aanmeldingen.txt", "a", encoding="utf-8") as file:
             file.write(f"Voornaam: {voornaam}\n")
             file.write(f"Achternaam: {achternaam}\n")
             file.write(f"Telefoon: {telefoon}\n")
@@ -76,3 +43,4 @@ def vrijwilligers():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
